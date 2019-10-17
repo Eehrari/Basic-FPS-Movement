d30d3a8f5eb111219c074e3ccf545096024c1deb
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2019-10-17 12:14:58 +0430

    First

---
+++ PlayerNameInputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class NewBehaviourScript : MonoBehaviour
+{
+ 
+    public void SetPlayerName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            print("Name is empty");
+            return;
+        }
+        PhotonNetwork.NickName = name;
+    }
+}
